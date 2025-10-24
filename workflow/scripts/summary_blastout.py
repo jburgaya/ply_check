fcdd272d19b56ac8c0158b2c09b8c2f0778a596d
Burgaya Ventura Judit (jb25u082) <jb25u082@submit02.ubelix.unibe.ch>
2025-10-24 16:28:47 +0200

    handle empty output files from blast

--- workflow/scripts/summary_blastout.py
+++ workflow/scripts/summary_blastout.py
@@ -1,52 +1,66 @@
 #!/usr/bin/env python
 
 def get_options():
-  import argparse
+    import argparse
 
-  description = "Summarize blast output into output df"
-  parser = argparse.ArgumentParser(description=description)
+    description = "Summarize blast output into output df"
+    parser = argparse.ArgumentParser(description=description)
 
-  parser.add_argument("--out-dir",
-                      help="Blast output dir")
-  parser.add_argument("--out-file",
-                      help="Output file to store results")
+    parser.add_argument("--out-dir",
+                        help="Blast output dir")
+    parser.add_argument("--out-file",
+                        help="Output file to store results")
 
-  return parser.parse_args()
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
-  options = get_options()
+    options = get_options()
 
-  import os
-  import glob
-  import pandas as pd
+    import os
+    import glob
+    import pandas as pd
 
-  cols = ["allele", "contig", "pident", "length", "qstart", "qend", "sstart", "send"]
+    cols = ["allele", "contig", "pident", "length", "qstart", "qend", "sstart", "send"]
 
-  records = []
+    records = []
 
-  for filepath in glob.glob(os.path.join(options.out_dir, "*.tsv")):
-      sample = os.path.basename(filepath).replace(".tsv", "")
-      df = pd.read_csv(filepath, sep="\t", names=cols)
-      df["sample"] = sample
+    for filepath in glob.glob(os.path.join(options.out_dir, "*.tsv")):
+        sample = os.path.basename(filepath).replace(".tsv", "")
+        df = pd.read_csv(filepath, sep="\t", names=cols)
 
-      best_hit = df.sort_values("pident", ascending=False).iloc[0]
+        if df.empty:
+            # Empty BLAST file → ply-NP
+            records.append({
+                "sample": sample,
+                "assigned_allele": "ply-NP",
+                "pident": "",
+                "alignment_length": "",
+                "contig": "",
+                "qstart": "",
+                "qend": "",
+                "sstart": "",
+                "send": ""
+            })
+            continue
 
-      records.append({
-          "sample": sample,
-          "assigned_allele": best_hit["allele"],
-          "pident": best_hit["pident"],
-          "alignment_length": best_hit["length"],
-          "contig": best_hit["contig"],
-          "qstart": best_hit["qstart"],
-          "qend": best_hit["qend"],
-          "sstart": best_hit["sstart"],
-          "send": best_hit["send"]
-      })
+        best_hit = df.sort_values("pident", ascending=False).iloc[0]
 
-  outfile = options.out_file
-  if not outfile.endswith(".tsv"):
-    outfile += ".tsv"
+        records.append({
+            "sample": sample,
+            "assigned_allele": best_hit["allele"],
+            "pident": best_hit["pident"],
+            "alignment_length": best_hit["length"],
+            "contig": best_hit["contig"],
+            "qstart": best_hit["qstart"],
+            "qend": best_hit["qend"],
+            "sstart": best_hit["sstart"],
+            "send": best_hit["send"]
+        })
 
-  result_df = pd.DataFrame(records)
-  result_df.to_csv(outfile, sep="\t", index=False)
+    outfile = options.out_file
+    if not outfile.endswith(".tsv"):
+        outfile += ".tsv"
+
+    result_df = pd.DataFrame(records)
+    result_df.to_csv(outfile, sep="\t", index=False)
